cbdf376792bd1ba72589df5c7e77facfc3d8114a
Keven Lavoie <keven.lavoie@imagemsoft.com>
2020-11-08 07:41:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react'
+
+import ReactDataGrid from '@inovua/reactdatagrid-community'
+import '@inovua/reactdatagrid-community/index.css'
+import Button from '@inovua/reactdatagrid-community/packages/Button'
+
+const gridStyle = { minHeight: 550, marginTop: 10 };
+
+const columns = [
+  { name: 'id', header: 'Id', defaultVisible: false, type: 'number', maxWidth: 40 },
+  { name: 'firstName', header: 'First Name', defaultFlex: 2 },
+  { name: 'lastName', header: 'Last Name', defaultFlex: 2 },
+  { name: 'email', header: 'Email', defaultFlex: 3 }
+];
+
 function App() {
+  const [dataSource, setDataSource] = React.useState([]);
+
+  const loadData = () => {
+    setDataSource([{ id: 1, firstName: 'test', lastName: "test1", email: 'email@test.com' }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Sort with remote data</h3>
+      <div>
+        <Button onClick={loadData}>Load async data</Button>
+        <Button
+          onClick={() => setDataSource([])}
+          style={{ marginLeft: 10 }}
+        >Clear data</Button>
+      </div>
+
+      <ReactDataGrid
+        idProperty="id"
+        style={gridStyle}
+        columns={columns}
+        dataSource={dataSource}
+      />
     </div>
   );
 }
